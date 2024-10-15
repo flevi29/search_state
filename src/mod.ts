70ec08d7d23701c13fd9b83de347aa8173350204
F. Levi <55688616+flevi29@users.noreply.github.com>
2024-10-15 15:56:15 +0300

    Update mod.ts

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,5 @@
 export * from "./search_state.ts";
-export * from "./search_box/search_box.ts";
+export * from "./search_box.ts";
 export * from "./hits_with_numbered_pagination.ts";
 export * from "./hits_with_pagination.ts";
 export * from "./sort_by.ts";
